84f33b9558854a81dc1335813fa923e5aa6d7902
hna12 <61971952+hna12@users.noreply.github.com>
2022-08-26 09:39:02 +0900

    Update cascade_mask_rcnn_x101_64x4d_fpn_1x_coco.py

--- Cascade_Mask_RCNN/cascade_mask_rcnn_x101_64x4d_fpn_1x_coco.py
+++ Cascade_Mask_RCNN/cascade_mask_rcnn_x101_64x4d_fpn_1x_coco.py
@@ -18,6 +18,15 @@ model = dict(
 # checkpoint = 'https://download.openmmlab.com/mmclassification/v0/resnet/resnet50_8xb256-rsb-a1-600e_in1k_20211228-20e21305.pth'  # noqa
 # model = dict(
 #     backbone=dict(
+#         type='ResNeXt',
+#         depth=101,
+#         groups=64,
+#         base_width=4,
+#         num_stages=4,
+#         out_indices=(0, 1, 2, 3),
+#         frozen_stages=1,
+#         norm_cfg=dict(type='BN', requires_grad=True),
+#         style='pytorch',
 #         init_cfg=dict(
 #             type='Pretrained', prefix='backbone.', checkpoint=checkpoint)))
 
